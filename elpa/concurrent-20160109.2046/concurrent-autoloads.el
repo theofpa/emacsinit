aa10fe26f55e3edea6520cfb793984a6412fe57f
Xin Jin <xjin@rbox.me>
2016-01-12 23:26:37 -0800

    update

---
+++ elpa/concurrent-20160109.2046/concurrent-autoloads.el
@@ -0,0 +1,15 @@
+;;; concurrent-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("concurrent.el") (22164 15523 551993 265000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; concurrent-autoloads.el ends here
---
+++ elpa/dash-20151216.1315/dash-autoloads.el
@@ -0,0 +1,15 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("dash.el") (22164 15522 652161 338000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; dash-autoloads.el ends here
