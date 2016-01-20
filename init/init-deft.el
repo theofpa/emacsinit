f8bd8d325f6743608c7238fe3bd417e0d2b52b17
Xin Jin <xjin@rbox.me>
2016-01-19 21:18:36 -0800

    deft-mode

--- init/init-deft.el
+++ init/init-deft.el
@@ -1,6 +1,18 @@
-(setq deft-extensions '("txt" "tex" "org"))
+(setq deft-extensions '("org" "tex" "txt"))
 (setq deft-directory "~/Dropbox/deft-notes")
 (setq deft-org-mode-title-prefix t)
+
+(defun deft-enter-close-last-window ()
+  (interactive)
+  (progn
+	(other-window 1)
+	(delete-window)
+	(deft-complete)))
+
+(defun deft-mode-local-keys ()
+  (local-set-key (kbd "C-m") 'deft-enter-close-last-window))
+
+(add-hook 'deft-mode-hook 'deft-mode-local-keys)
 
 
 (provide 'init-deft)
